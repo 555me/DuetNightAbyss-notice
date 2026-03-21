61e9fab6b38606921c5fd4231754f35c1bf6c55f
222 <cmude@github.com>
2026-03-21 06:57:48 +0000

    v2 & 更新 03-21 15.12

---
+++ data/notice_beta1.lua
@@ -0,0 +1,69 @@
+return {
+    ["697c899cf2c48b0b9b3408e4"] = {
+        ["UniqueId"] = "697c899cf2c48b0b9b3408e4",
+        ["StartTimestamp"] = 1769769360,
+        ["Title"] = "新版本保密测试活动时间说明",
+        ["EndTimestamp"] = 1774281599,
+        ["Channels"] = {
+        },
+        ["Content"] = {
+            [1] = {
+                ["language"] = "CN",
+                ["title"] = "新版本保密测试活动时间说明",
+                ["title1"] = "新版本保密测试活动时间说明",
+                ["body"] = "<p><img src=\"http://pan01-cdn-dna-ali.shyxhy.com/upload/重要通知.png\" alt=\"\" width=\"728\" height=\"200\"></p>\n<div data-page-id=\"G1CQdwukyoJ0ROxTKFKcgihvntf\" data-lark-html-role=\"root\" data-docx-has-block-data=\"false\">\n<div class=\"ace-line ace-line old-record-id-IceadURtRoeWjbxqaHncN1iPnge\">亲爱的狩月人：</div>\n<div class=\"ace-line ace-line old-record-id-GWkxdMOpToIBnSxxFbRclamynaf\">欢迎您参与体验《二重螺旋》先遣测试服！</div>\n<div class=\"ace-line ace-line old-record-id-R6gZdW4TRoHWSIxn9TLcpmwsnbd\">&nbsp;</div>\n<div class=\"ace-line ace-line old-record-id-NKSudKzVGo7bl2x65qGckGrjnzd\">在本次测试中，会提前开启后续即将在正式服上新的活动及商业化内容。</div>\n<div class=\"ace-line ace-line old-record-id-FfiadUXKco7gf2xJaYFcThrsnZg\">第一批上架活动计划如下：</div>\n<div class=\"ace-line ace-line old-record-id-PwIXdkk66osqGfxBYzacgiasn4d\">【元亨利贞】：2026年1月30日 开服后</div>\n<div class=\"ace-line ace-line old-record-id-DjDIdFJ1wox7mnx6MG6ctF5unkg\">【机傀大乱斗】：2026年1月30日 开服后</div>\n<div class=\"ace-line ace-line old-record-id-L9ewdOMZJoWkZfxwISzcS8usnfd\">【饰品上新】：2026年1月30日 开服后</div>\n<div class=\"ace-line ace-line old-record-id-U5qkdrBbfoGDsaxeczccsTXrnwY\">【星耀巡游】：2026年1月30日 10点（UTC+8）</div>\n<div class=\"ace-line ace-line old-record-id-AJ6gdgNp6o1u9zx2RivcoR9xnlb\">【收获日】：2026年2月6日 10点（UTC+8）</div>\n<div class=\"ace-line ace-line old-record-id-BsiAdBrzsomJkgx2qALcamGZnde\">【无由履险奇谭】：2026年2月13日 10点（UTC+8）</div>\n<div class=\"ace-line ace-line old-record-id-JtTKdCbCsocOitxSUgBchJTRnte\">&nbsp;</div>\n<div class=\"ace-line ace-line old-record-id-U0xfdGN92o2ggcxDIlUc1B0Dnvh\">第二批上架活动计划如下：</div>\n<div class=\"ace-line ace-line old-record-id-SSHsdbkyWofeWJxCL6scZMFCnWf\">【清平愿】：2026年2月24日 10点（UTC+8）</div>\n<div class=\"ace-line ace-line old-record-id-CFQLd9vR9oJ3dfx7ouwchBknn58\">【文明博弈】：2026年2月27日 10点（UTC+8）</div>\n<div class=\"ace-line ace-line old-record-id-TvrNddgZooyawTxhTe7cYku8nbg\">【收获日】：2026年3月6日 10点（UTC+8）</div>\n<div class=\"ace-line ace-line old-record-id-Krcud3EHgouSbSxYzsVcVHK3nxh\">【狩月人之阶】：2026年3月10日 10点（UTC+8）</div>\n<div class=\"ace-line ace-line old-record-id-Xs3XdqcEjoEtnXxD76gck8L2nDh\">【百变奇妙夜】：2026年3月13日 10点（UTC+8）</div>\n<div class=\"ace-line ace-line old-record-id-AoE4ds8E7oFaIcxyJbZcux9SnPh\">&nbsp;</div>\n<div class=\"ace-line ace-line old-record-id-OwM9dgw6Hov6cyx9ycWcjsYRnaf\">再次感谢您的参与和理解，我们会持续收听大家的建议与反馈，继续优化游戏品质！</div>\n<div class=\"ace-line ace-line old-record-id-AOzjdTEj2oMzvcxQCLBcXihmnlf\">《二重螺旋》运营团队</div>\n</div>",
+            },
+        },
+        ["ClientOnly"] = {
+            ["UIStyle"] = "1",
+            ["notice_sort"] = 1000,
+            ["noticeType"] = "1,2",
+            ["TypeTag"] = "1",
+        },
+    },
+    ["697ca347f2c48b0b9b341207"] = {
+        ["UniqueId"] = "697ca347f2c48b0b9b341207",
+        ["StartTimestamp"] = 1764591906,
+        ["Title"] = "问题反馈方式",
+        ["EndTimestamp"] = 1798732800,
+        ["Channels"] = {
+        },
+        ["Content"] = {
+            [1] = {
+                ["language"] = "CN",
+                ["title"] = "问题反馈方式",
+                ["title1"] = "问题反馈方式",
+                ["body"] = "<p><img src=\"http://pan01-cdn-dna-ali.shyxhy.com/upload/公告头图11-功能说明-问题反馈方式 (1).png\" alt=\"\" width=\"728\" height=\"200\"></p>\n<div data-page-id=\"MgbSdRnQvogpK7xgRe1cKJvcnEd\" data-lark-html-role=\"root\" data-docx-has-block-data=\"false\">\n<div class=\"ace-line ace-line old-record-id-doxcnzv4bQlPQFVvLDHBc7lwRZg\">亲爱的狩月人：</div>\n<div class=\"ace-line ace-line old-record-id-doxcnOy0UbMWVzKVqXsZLmnNxig\">「先遣测试服」现已开启，狩月人在测试过程中遇到任何问题或建议想法，都可以通过以下方式进行反馈。</div>\n<div class=\"ace-line ace-line old-record-id-doxcnsex2SZdta8d9CifLEnqZag\">&nbsp;</div>\n<div class=\"ace-line ace-line old-record-id-doxcn4wJmTB3cMQYN1fZIADHJ4b\"><strong>★建议与BUG反馈方式★</strong></div>\n<div class=\"ace-line ace-line old-record-id-doxcn0IfDA8vcW0nuwflFMmPb5H\">✦ 问卷填写</div>\n<div class=\"ace-line ace-line old-record-id-doxcnzj0CBlMqeaVv72ZfoFXKfh\">1、进入反馈问卷地址【<a href=\"https://herogames.feishu.cn/share/base/form/shrcnDaciS7DO7o9Br2aWLPzYAN\" data-lark-is-custom=\"true\">游戏建议</a>】【<a href=\"https://herogames.feishu.cn/share/base/form/shrcnAbBgXyANkq7bzHLt6Etesc\" data-lark-is-custom=\"true\">BUG反馈</a>】</div>\n<div class=\"ace-line ace-line old-record-id-doxcnYdJ47I0qpD9gPvAIu4qZJb\">2、填写反馈内容（如截图无法直接看出问题，希望可以附上复现的录屏）</div>\n<div class=\"ace-line ace-line old-record-id-doxcn2lPo18F3qIexVez9OFi5Wg\">3、填写设备信息与联系方式</div>\n<div class=\"ace-line ace-line old-record-id-doxcndOMvWOauAYyQ1fKxDUi7Sc\">&nbsp;</div>\n<div class=\"ace-line ace-line old-record-id-doxcnsgsVs1iVPigWRic8w4BBvx\">欢迎各位狩月人踊跃提供意见和建议，帮助我们持续优化游戏体验，共同建设一个更好的《二重螺旋》。</div>\n<div class=\"ace-line ace-line old-record-id-doxcnpf1l7oeQ3NseR7lWHYFcnc\">&nbsp;</div>\n<div class=\"ace-line ace-line old-record-id-doxcnsXgT9FlNbRQljwCi7wgObf\">《二重螺旋》运营团队</div>\n</div>",
+            },
+        },
+        ["ClientOnly"] = {
+            ["UIStyle"] = "1",
+            ["notice_sort"] = 1000,
+            ["noticeType"] = "1,2",
+            ["TypeTag"] = "2",
+        },
+    },
+    ["Version"] = 4,
+    ["697c8989f2c48b0b9b3408e3"] = {
+        ["UniqueId"] = "697c8989f2c48b0b9b3408e3",
+        ["StartTimestamp"] = 1769769335,
+        ["Title"] = "问题反馈方式",
+        ["EndTimestamp"] = 1774281599,
+        ["Channels"] = {
+        },
+        ["Content"] = {
+            [1] = {
+                ["language"] = "CN",
+                ["title"] = "问题反馈方式",
+                ["title1"] = "问题反馈方式",
+                ["body"] = "<p><img src=\"http://pan01-cdn-dna-ali.shyxhy.com/upload/公告头图11-功能说明-问题反馈方式 (1).png\" alt=\"\" width=\"728\" height=\"200\"></p>\n<div data-page-id=\"MgbSdRnQvogpK7xgRe1cKJvcnEd\" data-lark-html-role=\"root\" data-docx-has-block-data=\"false\">\n<div class=\"ace-line ace-line old-record-id-doxcnzv4bQlPQFVvLDHBc7lwRZg\">亲爱的狩月人：</div>\n<div class=\"ace-line ace-line old-record-id-doxcnOy0UbMWVzKVqXsZLmnNxig\">「1.2先遣服」现已开启，狩月人在测试过程中遇到任何问题或建议想法，都可以通过以下方式进行反馈。</div>\n<div class=\"ace-line ace-line old-record-id-doxcnsex2SZdta8d9CifLEnqZag\">&nbsp;</div>\n<div class=\"ace-line ace-line old-record-id-doxcn4wJmTB3cMQYN1fZIADHJ4b\"><strong>★建议与BUG反馈方式★</strong></div>\n<div class=\"ace-line ace-line old-record-id-doxcn0IfDA8vcW0nuwflFMmPb5H\">✦ 问卷填写</div>\n<div class=\"ace-line ace-line old-record-id-doxcnzj0CBlMqeaVv72ZfoFXKfh\">1、进入反馈问卷地址【<a href=\"https://herogames.feishu.cn/share/base/form/shrcnDaciS7DO7o9Br2aWLPzYAN\" data-lark-is-custom=\"true\">游戏建议</a>】【<a href=\"https://herogames.feishu.cn/share/base/form/shrcnAbBgXyANkq7bzHLt6Etesc\" data-lark-is-custom=\"true\">BUG反馈</a>】</div>\n<div class=\"ace-line ace-line old-record-id-doxcnYdJ47I0qpD9gPvAIu4qZJb\">2、填写反馈内容（如截图无法直接看出问题，希望可以附上复现的录屏）</div>\n<div class=\"ace-line ace-line old-record-id-doxcn2lPo18F3qIexVez9OFi5Wg\">3、填写设备信息与联系方式</div>\n<div class=\"ace-line ace-line old-record-id-doxcndOMvWOauAYyQ1fKxDUi7Sc\">&nbsp;</div>\n<div class=\"ace-line ace-line old-record-id-doxcnsgsVs1iVPigWRic8w4BBvx\">欢迎各位狩月人踊跃提供意见和建议，帮助我们持续优化游戏体验，共同建设一个更好的《二重螺旋》。</div>\n<div class=\"ace-line ace-line old-record-id-doxcnpf1l7oeQ3NseR7lWHYFcnc\">&nbsp;</div>\n<div class=\"ace-line ace-line old-record-id-doxcnsXgT9FlNbRQljwCi7wgObf\">《二重螺旋》运营团队</div>\n</div>",
+            },
+        },
+        ["ClientOnly"] = {
+            ["UIStyle"] = "1",
+            ["notice_sort"] = 1000,
+            ["noticeType"] = "1,2",
+            ["TypeTag"] = "2",
+        },
+    },
+}
---
+++ data/notice_beta2.lua
@@ -0,0 +1,47 @@
+return {
+    ["693c0c04dc96701982047cf3"] = {
+        ["EndTimestamp"] = 1775824502,
+        ["Content"] = {
+            [1] = {
+                ["title1"] = "问题反馈方式",
+                ["body"] = "<div data-page-id=\"YuAxdr17eorZFNx9vSZcLn4wnTh\" data-lark-html-role=\"root\" data-docx-has-block-data=\"false\">\n<div class=\"ace-line ace-line old-record-id-doxcnQoAqdldBHCyIWSQGpB1DHh\"><img src=\"http://pan01-cdn-dna-ali.shyxhy.com/upload/99-问题反馈方式.PNG\" alt=\"\" width=\"728\" height=\"200\"></div>\n<div class=\"ace-line ace-line old-record-id-doxcnQoAqdldBHCyIWSQGpB1DHh\">&nbsp;</div>\n<div class=\"ace-line ace-line old-record-id-doxcnQoAqdldBHCyIWSQGpB1DHh\">亲爱的狩月人：</div>\n<div class=\"ace-line ace-line old-record-id-doxcntrZssCrfJotftl7kVJrPWb\">「华胥先遣服」现已开启，狩月人在测试过程中遇到任何问题或建议想法，都可以通过以下方式进行反馈。</div>\n<div class=\"ace-line ace-line old-record-id-doxcngxEPFJ77QkrkIQoz34gNPf\">&nbsp;</div>\n<div class=\"ace-line ace-line old-record-id-doxcn9uNctNLyp9oQ7DUWQr3buc\"><strong>★建议与BUG反馈方式★</strong></div>\n<div class=\"ace-line ace-line old-record-id-doxcn3Eedz2LrSVLnCbYSvfb9hh\">✦ 问卷填写</div>\n<div class=\"ace-line ace-line old-record-id-doxcnd81rlgNpfhU6WLiYMpX6oh\">1、进入反馈问卷地址【<a href=\"https://herogames.feishu.cn/share/base/form/shrcnRiCgCBhq1cu7W9EQ4kHXqc\" data-lark-is-custom=\"true\">游戏建议</a>】【<a href=\"https://herogames.feishu.cn/share/base/form/shrcnxAzPapfb3FvfwaehQvl0Yf\" data-lark-is-custom=\"true\">BUG反馈</a>】</div>\n<div class=\"ace-line ace-line old-record-id-doxcnTz4T2tVqPmknZ7ZuHrKedg\">2、填写反馈内容（如截图无法直接看出问题，希望可以附上复现的录屏）</div>\n<div class=\"ace-line ace-line old-record-id-doxcnyxALQRjBsVoj3ZPpvXGM2g\">3、填写设备信息与联系方式</div>\n<div class=\"ace-line ace-line old-record-id-doxcn1tGYTUonJrO8s99NULPqme\">&nbsp;</div>\n<div class=\"ace-line ace-line old-record-id-doxcntqh7LZB4fKdprU3WEM37nd\">欢迎各位狩月人踊跃提供意见和建议，帮助我们持续优化游戏体验，共同建设一个更好的《二重螺旋》。</div>\n<div class=\"ace-line ace-line old-record-id-doxcnMnfxT5elthxeU9VyC2RvWc\">&nbsp;</div>\n<div class=\"ace-line ace-line old-record-id-doxcnFgockRsi6yMevG0yZpjfLd\">《二重螺旋》运营团队</div>\n</div>",
+                ["title"] = "问题反馈方式",
+                ["language"] = "CN",
+            },
+        },
+        ["StartTimestamp"] = 1765542899,
+        ["Channels"] = {
+        },
+        ["UniqueId"] = "693c0c04dc96701982047cf3",
+        ["Title"] = "问题反馈方式",
+        ["ClientOnly"] = {
+            ["notice_sort"] = 1,
+            ["TypeTag"] = "2",
+            ["UIStyle"] = "1",
+            ["noticeType"] = "1,2",
+        },
+    },
+    ["Version"] = 13,
+    ["693bf9efdc96701982047ce4"] = {
+        ["EndTimestamp"] = 1776424678,
+        ["Content"] = {
+            [1] = {
+                ["title1"] = "【皎皎的支援】测试特别活动",
+                ["body"] = "<div data-page-id=\"FthcdjiX2oLu9FxcjrKcWZ5knGg\" data-lark-html-role=\"root\" data-docx-has-block-data=\"false\">\n<div class=\"ace-line ace-line old-record-id-HG8EdAdBqoTEDuxMO1zchGBinRL\"><img src=\"http://pan01-cdn-dna-ali.shyxhy.com/upload/皎皎的特别支援.PNG\" alt=\"\" width=\"728\" height=\"200\"></div>\n<div class=\"ace-line ace-line old-record-id-HG8EdAdBqoTEDuxMO1zchGBinRL\">&nbsp;</div>\n<div class=\"ace-line ace-line old-record-id-HG8EdAdBqoTEDuxMO1zchGBinRL\">亲爱的狩月人：</div>\n<div class=\"ace-line ace-line old-record-id-CJ9ddcHGio7HyTxFEFmczcokntc\">测试期间，为了给各位狩月人带来更愉快的游戏体验，我们将发放以下邮件内容，感谢对二重螺旋的关注与支持。</div>\n<div class=\"ace-line ace-line old-record-id-PZqBdxOenoFSfPxCi6Gcyaamnbf\">&nbsp;</div>\n<div class=\"ace-line ace-line old-record-id-YwRNdbVUBoKFjEx8wWyc6fShn4f\">◆ 测试感谢礼</div>\n<div class=\"ace-line ace-line old-record-id-LJWCd31ruoe8YLxtusSc1ly6naf\">为感谢狩月人参与本次测试，解锁邮件系统后，可通过邮件直接领取「月石晶胚」*100000、「虹色棱镜」*1000。</div>\n<div class=\"ace-line ace-line old-record-id-Wpe5dz7ugozbR8x6FXbcVydFnAc\">&nbsp;</div>\n<div class=\"ace-line ace-line old-record-id-AslzdrbPhoByQ9xaYxGcspHyn6c\">随着测试进度，后续会逐渐新增额外的测试资源，敬请期待。</div>\n<div class=\"ace-line ace-line old-record-id-Do2Fdxla1o46jBxXEpWc0h3cnLh\">&nbsp;</div>\n<div class=\"ace-line ace-line old-record-id-JDLVd7kXxoT7CJxZfxqcOoc0nCd\">《二重螺旋》运营团队</div>\n</div>",
+                ["title"] = "【皎皎的支援】测试特别活动",
+                ["language"] = "CN",
+            },
+        },
+        ["StartTimestamp"] = 1765538273,
+        ["Channels"] = {
+        },
+        ["UniqueId"] = "693bf9efdc96701982047ce4",
+        ["Title"] = "【皎皎的支援】测试特别活动",
+        ["ClientOnly"] = {
+            ["notice_sort"] = 8000,
+            ["TypeTag"] = "1",
+            ["UIStyle"] = "1",
+            ["noticeType"] = "1,2",
+        },
+    },
+}
